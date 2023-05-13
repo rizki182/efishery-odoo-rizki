64a04ccf75cab250c25af13049ccaea053b19c90
Rizki <rizkifaishal182@gmail.com>
2023-05-13 15:44:12 +0700

    feat(interceptor): caching
    Implement caching for sale order list

--- interceptor/code/models/sale_order.js
+++ interceptor/code/models/sale_order.js
@@ -5,13 +5,33 @@ const cache = new NodeCache( { stdTTL: 3600, checkperiod: 600 } );
 module.exports = {
   async index(){
     try{
-      // fetch data from API
-      const response = await axios.get('http://web:8069/custom_api/sales_order',{
-        headers: {
-          "API-KEY": "25b2b4378ad81a58b422247da757eedebcd15252"
-        }
-      });
-      return response.data;
+      // final response
+      let response
+
+      // check cached data
+      let cached_list = cache.get("sale_order_list");
+      if ( cached_list == undefined ){
+        // fetch data from odoo if cache is empty
+        const res = await axios.get('http://web:8069/custom_api/sales_order',{
+          headers: {
+            "API-KEY": "25b2b4378ad81a58b422247da757eedebcd15252"
+          }
+        });
+
+        // save to cache
+        cache.set("sale_order_list", JSON.stringify(res.data));
+
+        // set final response
+        response = res.data;
+        console.log("from api");
+      } else {
+        // set final response
+        response = JSON.parse(cached_list);
+        console.log("from cache");
+      }
+
+
+      return response;
     } catch (err) {
       throw new Error(err);
     }
